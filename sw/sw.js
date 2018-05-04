288782e1784a939335f8866fca4c9fe01dae3c4a
jarjune <408100801@qq.com>
2018-05-04 11:06:14 +0800

    pwa

---
+++ sw/sw.js
@@ -0,0 +1,33 @@
+var version = 'jarjune_v2018_05_04';
+
+var list = [
+	'/',
+	'index.html'
+];
+
+self.addEventListener('install', function(e) {
+	e.waitUntil(
+		caches.open(version).then(function(cache) {
+		return cache.addAll(list)
+	}))
+})
+
+self.addEventListener('activate', function(e) {
+	e.waitUntil(caches.keys().then(function(cacheNames) {
+		return Promise.all(
+			cacheNames.map(function(cacheName) {
+			if(cacheName !== version) 
+				return caches.delete(cacheName)
+		}))
+	}))
+})
+
+self.addEventListener('fetch', function(e) {
+	e.respondWidth(
+		caches.match(e.request).then(function(response) {
+			if(response != null)
+				return response;
+			return fetch(e.request.url)
+		})
+	)
+})
